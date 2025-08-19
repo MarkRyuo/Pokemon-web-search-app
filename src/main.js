6875e7d6acf0099995acf4e3703fa2e38dc3b21d
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-08-19 10:58:46 +0800

    Subject: Message 08/19/25

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    
+    <input type="text" name="searchPokemon" placeholder="Search a Pokemon!" id="inptPokemon">
+    <button type="button" id="btnSearch">
+      Search
+    </button>
   </div>
 `
 

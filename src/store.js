8db4f4e7f7f8d00f02ababd3a6166555a82cba49
MarkRyuo <malupajhonmark3035@gmail.com>
2025-08-23 22:19:53 +0800

    Subject: Update 08/23/25

--- src/store.js
+++ src/store.js
@@ -3,15 +3,23 @@
 export const storeData = {
     state : {
         pokemonData: null, // will hold the full Pokémon data fetched from the API.
-        savedDate: null
+        lastSearch: null // will hold the name of the last searched Pokémon.
     }, // state is the internal storage of the Store.
 
     setPokemon(data) {
-        this.state.pokemonData = data ;
+        this.state.pokemonData = data ; 
     },
 
     getPokemon(){
         return this.state.pokemonData
+    },
+
+    setLastSearch(data){
+        this.state.lastSearch = data ;
+    },
+
+    getLastSearch(){
+        return this.state.lastSearch
     }
 }
 
